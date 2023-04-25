aefeeebe57f49ea581217362fa2ac7e4719c36cf
Lerato-Lee <mthimkhululerato72@gmail.com>
2023-04-25 16:47:26 +0000

    bug fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,24 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
+const filePath = process.argv[3];
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const filmIndex in films) {
-      const filmChars = films[filmIndex].characters;
-      for (const charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    fs.writeFile(filePath, body, 'utf-8');
   }
 });
